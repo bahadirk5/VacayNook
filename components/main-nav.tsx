af906812245dd744cf063dd915eb214f04ae21bb
bahadirk5 <korkmazerb0@gmail.com>
2023-06-09 17:51:27 +0300

    mobil menu logo

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -13,11 +13,9 @@ interface MainNavProps {
 export function MainNav({ items }: MainNavProps) {
   return (
     <div className="flex gap-6 md:gap-10">
-      <Link href="/" className="hidden items-center space-x-2 md:flex">
+      <Link href="/" className="items-center space-x-2 flex">
         <Icons.logo className="h-6 w-6" />
-        <span className="hidden font-bold sm:inline-block">
-          {siteConfig.name}
-        </span>
+        <span className="font-bold inline-block">{siteConfig.name}</span>
       </Link>
       {items?.length ? (
         <nav className="hidden gap-6 md:flex">
